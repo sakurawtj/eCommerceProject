d44125dc768120477832f380445a6a086fe3190c
sakurawtj <53387760+sakurawtj@users.noreply.github.com>
2023-08-01 16:32:55 -0700

    implement product backend

--- backend/ecommerce/src/main/java/com/sakurawtj/ecommerce/config/MyDataRestConfig.java
+++ backend/ecommerce/src/main/java/com/sakurawtj/ecommerce/config/MyDataRestConfig.java
@@ -1,2 +1,27 @@
-package com.sakurawtj.ecommerce.config;public class MyDataRestConfig {
+package com.sakurawtj.ecommerce.config;
+
+import com.sakurawtj.ecommerce.entity.Product;
+import com.sakurawtj.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+        // disable HTTP methods for Product and ProductCategory: PUT, POST, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
+
+    }
 }
--- backend/ecommerce/src/main/java/com/sakurawtj/ecommerce/dao/ProductRepository.java
+++ backend/ecommerce/src/main/java/com/sakurawtj/ecommerce/dao/ProductRepository.java
@@ -1,2 +1,10 @@
-package com.sakurawtj.ecommerce.dao;public interface ProductRepository {
+package com.sakurawtj.ecommerce.dao;
+
+import com.sakurawtj.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
