50085bcbf3971f8c92ef87b37143a8f61fa6bba7
sakurawtj <53387760+sakurawtj@users.noreply.github.com>
2023-08-08 00:02:02 -0700

    dao implemented

--- backend/ecommerce/src/main/java/com/sakurawtj/ecommerce/dao/CustomerRepository.java
+++ backend/ecommerce/src/main/java/com/sakurawtj/ecommerce/dao/CustomerRepository.java
@@ -1,2 +1,8 @@
-package com.sakurawtj.ecommerce.dao;public class CustomerRepository {
+package com.sakurawtj.ecommerce.dao;
+
+import com.sakurawtj.ecommerce.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
 }
